15c457e63c7e2c8aaf6cdde21f97c44e855632d3
yuta-plasmamed <mtmt18ocmu@dmail.com>
2021-12-08 17:00:35 +0900

    saiteishutu

--- matumoto-y/homework-2-y.py
+++ matumoto-y/homework-2-y.py
@@ -8,13 +8,15 @@ Created on Fri Dec  3 16:28:11 2021
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt                                            
-    
+
+'''    
 V = np.loadtxt(
        fname="C12021-11-25-He-1kV-on-100000.txt" ,
        dtype="float" ,
        delimiter="\t" ,
        skiprows=5 ,
        )
+'''
 
 I = np.loadtxt(
        fname="C22021-11-25-He-1kV-on-100000.txt" ,
@@ -24,17 +26,39 @@ I = np.loadtxt(
        )
 
 w = 1200
+#w = int(input('w='))
+
 
 peaks1 = signal.argrelmax(I[:,1], order=w)
 peaks2 = signal.argrelmin(I[:,1], order=w)
 
+peak = []
+for i in peaks1[0]:
+    peak.append(i)
+    
+for i in peaks2[0]:
+    peak.append(i)
+peak = sorted(peak)
+print(peak)
+
 plt.plot(I[:,0], I[:,1], color='black')
+plt.scatter(I[:,0][peaks1], I[:,1][peaks1],c = 'r')
+plt.scatter(I[:,0][peaks2], I[:,1][peaks2],c = 'b')
 plt.xlabel("Time(s)")
 plt.ylabel("Current(A)")
-plt.plot(I[:,0][peaks1], I[:,1][peaks1],"ro")
-plt.plot(I[:,0][peaks2], I[:,1][peaks2],"bo")
+
+peakpeak = []
+
+for i in range(len(peak)-1):
+    aveave = (peak[i]+peak[i+1])//2
+    peakpeak.append(aveave)
+    
+print(aveave)
+
+Is = np.split(I, peakpeak)
+
+for i in range(len(peak)):
+    plt.plot(Is[i][:,0],Is[i][:,1])
 
 
-Is = np.split(I, [977,1982,2481,3453,4453,4941,5947,6947,7445,8417,9417])
 
-print(Is)
